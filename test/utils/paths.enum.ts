25d554dd0116b0c44e5cf875f57c39f3469f0287
reyhane-code <reyhane.code@gmail.com>
2024-07-14 13:01:05 +0330

    feat(test): replace sequlize in pool and context

---
+++ test/utils/paths.enum.ts
@@ -0,0 +1,11 @@
+export enum MigrationPaths {
+  USERS = '../../migrations/20240704062222-users.js',
+  PUBLISHERS = '../../migrations/20240706111000-publishers.js',
+  GENERS = '../../migrations/20240706111010-genres.js',
+  PLATFORMS = '../../migrations/20240706111022-platforms.js',
+  GAMES = '../../migrations/20240706111037-games.js',
+  PUBLISHER_GAMES = '../../migrations/20240707100211-publisher_games.js',
+  GENER_GAMES = '../../migrations/20240707100219-genres_games.js',
+  PLATFORM_GAMES = '../../migrations/20240707100231-platform_games.js',
+  LIKES = '../../migrations/20240709083739-likes.js',
+}
