f3f3dcdccd8cbc9e93b0588f7630d652c75899c8
reyhane-code <reyhane.code@gmail.com>
2024-07-18 11:19:29 +0330

    feat(games): create addGameToRelationTables

--- src/games/games.providers.ts
+++ src/games/games.providers.ts
@@ -1,9 +1,24 @@
-import { Game } from "models/game.model";
-import { Repositories } from "src/enums/database.enum";
+import { Game } from 'models/game.model';
+import { GenreGame } from 'models/genre_game.model';
+import { PlatformGame } from 'models/platform_game.model';
+import { PublisherGame } from 'models/publisher_game.model';
+import { Repositories } from 'src/enums/database.enum';
 
 export const gamesProviders = [
   {
     provide: Repositories.GAMES,
     useValue: Game,
+  },
+  {
+    provide: Repositories.PLATFORM_GAMES,
+    useValue: PlatformGame,
+  },
+  {
+    provide: Repositories.PUBLISHER_GAMES,
+    useValue: PublisherGame,
+  },
+  {
+    provide: Repositories.GENRE_GAMES,
+    useValue: GenreGame,
   },
 ];
