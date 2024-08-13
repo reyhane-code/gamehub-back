9838ec1954bd205d78d4613d9e6ff17fd4691944
reyhane-code <reyhane.code@gmail.com>
2024-08-13 12:18:08 +0330

    fix(article): files usage in articles.service

--- src/articles/articles.module.ts
+++ src/articles/articles.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ArticlesController } from './articles.controller';
 import { ArticlesService } from './articles.service';
 import { articlesProviders } from './articles.providers';
+import { FilesService } from 'src/files/files.service';
 
 @Module({
   controllers: [ArticlesController],
-  providers: [ArticlesService, ...articlesProviders],
+  providers: [ArticlesService, FilesService, ...articlesProviders],
 })
 export class ArticlesModule {}
