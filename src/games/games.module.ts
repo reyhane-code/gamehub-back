33d873c95ed4e23961753e6af0df0bb1bb468262
reyhane-code <reyhane.code@gmail.com>
2024-08-13 12:15:16 +0330

    feat(games): games-helper service

--- src/games/games.module.ts
+++ src/games/games.module.ts
@@ -3,10 +3,11 @@ import { GamesController } from './games.controller';
 import { GamesService } from './games.service';
 import { gamesProviders } from './games.providers';
 import { FilesModule } from 'src/files/files.module';
+import { GameHelperService } from './games-helper.service';
 
 @Module({
   controllers: [GamesController],
-  providers: [GamesService, ...gamesProviders],
+  providers: [GamesService, GameHelperService, ...gamesProviders],
   imports: [FilesModule],
 })
 export class GamesModule {}
