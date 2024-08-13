08f61fd3679be63d95cf794024e9afd45b86e2fa
reyhane-code <reyhane.code@gmail.com>
2024-08-13 23:55:49 +0330

    feat(articles): return likes count

--- src/articles/articles.module.ts
+++ src/articles/articles.module.ts
@@ -3,9 +3,12 @@ import { ArticlesController } from './articles.controller';
 import { ArticlesService } from './articles.service';
 import { articlesProviders } from './articles.providers';
 import { FilesService } from 'src/files/files.service';
+import { LikesModule } from 'src/likes/likes.module';
+import { FilesModule } from 'src/files/files.module';
 
 @Module({
   controllers: [ArticlesController],
   providers: [ArticlesService, FilesService, ...articlesProviders],
+  imports: [FilesModule, LikesModule],
 })
 export class ArticlesModule {}
--- src/articles/articles.providers.ts
+++ src/articles/articles.providers.ts
@@ -1,14 +1,9 @@
 import { Article } from 'models/article.model';
-import { File } from 'models/file.model';
 import { Repository } from 'src/enums/database.enum';
 
 export const articlesProviders = [
   {
     provide: Repository.ARTICLES,
     useValue: Article,
-  },
-  {
-    provide: Repository.FILES,
-    useValue: File,
   },
 ];
